acd0ff8741db1f34bc88f473f7bec21560d6903f
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-05-15 22:52:51 +0530

    add get, post, put, update, delete methods

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
 const http = require("http");
+const PORT = 3000;
+
+let shelf = [
+    { id : 1, name: "Song Of Achilles", rating: 5, author: "Madeline Miller", status: "read" },
+    { id: 2, name: "My Dark Vanessa", rating: 5, author: "Kate Elizabeth Russell", status: "read" },
+    { id: 3, name: "Frankenstein", rating: 0, author: "Mary Shelley", status: "unread" }
+];
+
+const server = http.createServer((req, res) => {
+
+    //GET
+    if (req.method === "GET" && req.url === "/getBooks") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify(books))
+    }
+
+    //POST
+    if (req.method === "POST" && req.url === "/createShelf") {
+        
+        let body = "";
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+    });
+
+        req.on("end", () => {
+            const newBook = json.parse(body)
+
+            newBook.id = shelf.length + 1;
+
+            shelf.push(newBook);
+
+            res.writeHead(201, { "Content-Type" : "application/json" });
+            res.end(JSON.stringify(newBook));
+        });
+        
+        return;
+    }
+ 
+    //PUT
+    if (req.method === "POST" && req.url === "/updateBooks/") {
+        
+        const id = parseInt(req.url.split("/")[2]);
+
+        let body = "";
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            const updatedBook = json.parse(body);
+            const book = shelf.find(u => u.id === id);
+
+            if (!book) {
+                res.writeHead(404);
+                return res.end("Entry not found!");
+            }
+
+            book.name = updatedBook.name || book.name
+            book.rating = updatedBook.rating || book.rating
+            book.author = updatedBook.author || book.author
+            book.status = updatedBook.status || book.status
+
+            res.writeHead(200, { "Content-Type" : "application/json"});
+            res.end(JSON.stringify(book));
+        });
+
+        return;
+    }
+
+    //DELETE
+    if (req.method === "DELETE" && req.url === "/deleteBooks/") {
+
+        const id = parseInt(req.url.split("/")[2]);
+
+        shelf = shelf.filter(u => u.id !== id);
+
+        res.writeHead(200);
+        res.end("Book deleted");
+
+        return;
+    }
+
+    //Route not found
+    res.writeHead(404);
+    res.end("Route not found");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
